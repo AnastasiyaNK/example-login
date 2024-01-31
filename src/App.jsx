be6990dce74b21418e7f150253b4f83705bc5dbc
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2024-01-31 14:46:29 +0100

    add pages Home and Welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Layout from 'Layout';
-import Header from 'components/Header/Header';
+
 import RestrictedRoute from 'components/RestrictedRoute/RestrictedRoute';
+import HomePage from 'pages/HomePage/HomePage';
 import LoginPage from 'pages/LogsnPage/LoginPage';
 import RegisterPage from 'pages/RegisterPage/RegisterPage';
+import WelcomePage from 'pages/WelcomePage/WelcomePage';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
@@ -10,6 +12,8 @@ export const App = () => {
   return (
     <Layout>
       <Routes>
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route
           path="/signup"
           element={
